3b06cbde108addc61d7ce8f7248e2ab38cb8e060
Akil-B <82441240+Akil-B@users.noreply.github.com>
2023-10-13 22:38:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    
+    canvas= createCreate(550,500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
+    }
+function draw(){
+    background('#969A97');
+}
+function modelLoaded(){
+
+    console.log('PoseNet Is Initialised!');
+
+}
+
+function gotPoses(){
+if(result.length > 0)
+{
+    console.log(results);
+}
+
+}
